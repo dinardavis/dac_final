057b479f0aa5a42b9ab35a040f4f9c3d45f05cf6
Dinar Davis <dinardavis@gmail.com>
2024-05-21 16:19:10 -0700

    Site content update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import PageNotFound from './components/pages/PageNotFound';
 function App() {
 
   return (
-    <BrowserRouter basename="/dac_final">
+    <BrowserRouter basename="/">
         <ScrollToTop />
         <Routes>
           <Route element={<Layout />}>
